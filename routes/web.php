10f15aa26d6242b824ab88dc3b7742d5fc6b591a
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-12-31 10:23:01 +0900

    node.js・bootstrapのインストール

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('tests/test', 'TestController@index');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');

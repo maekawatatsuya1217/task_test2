922576c3d82d969476595f204d99e98910f1b675
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-12-31 20:22:18 +0900

    index.blade.phpを表示

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::get('tests/test', 'TestController@index');
+
+Route::group(['prefix' => 'contact', 'middleware' => 'auth'], function(){
+    Route::get('index', 'ContactFormController@index')->name('contact.index');
+});
+
+// Route::resource('contacts', 'ContactFormController');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');

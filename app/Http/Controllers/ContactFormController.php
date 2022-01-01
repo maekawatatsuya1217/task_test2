f465bec6f2fe33451abd145d21f201a4ff0781b6
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2022-01-01 15:41:13 +0900

    DBのデータ取得

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\ContactForm;
+use Illuminate\Support\Facades\DB;
 
 class ContactFormController extends Controller
 {
@@ -14,7 +14,16 @@ class ContactFormController extends Controller
      */
     public function index()
     {
-        return view('contact.index');
+        
+        // $contacts = ContactForm::all();
+
+        $contacts = DB::table('contact_forms')
+        ->select('id', 'your_name', 'title', 'created_at')
+        ->orderBy('created_at', "desc")
+        ->get();
+        // dd($contacts);
+
+        return view('contact.index', compact('contacts'));
     }
 
     /**

21e371dcd57bafd9f6b13f7e7c20734c0a38721a
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2022-01-01 16:07:13 +0900

    詳細ページを作成

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -72,7 +72,8 @@ class ContactFormController extends Controller
      */
     public function show($id)
     {
-        //
+        $contact = ContactForm::find($id);
+        return view('contact.show', compact('contact'));
     }
 
     /**
---
+++ resources/views/contact/show.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    showです
+                    {{ $contact->your_name}}
+                    <form method="GET" action="">
+                        <!-- Laravelでフォームを作成する際は必ず@csrfの記述が必要 -->
+                        @csrf
+                        <!-- Laravelでフォームを作成する際は必ず@csrfの記述が必要 -->
+                        <input class="btn btn-info" type="submit" value="変更する">
+                    </form>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection

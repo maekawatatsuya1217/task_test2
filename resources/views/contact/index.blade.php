8e9e46f0bc7df87ce165ca9a4fad1fa55d51587e
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2022-01-01 10:49:07 +0900

    DBにフォーム情報を保存

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -15,6 +15,12 @@
                     @endif
 
                     indexです
+                    <!-- ボタン -->
+                    <form method="GET" action="{{ route('contact.create') }}">
+                        <button type="submit" class="btn btn-primary">
+                            新規登録
+                        </button>
+                    </form>
                 </div>
             </div>
         </div>

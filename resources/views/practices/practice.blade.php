c4f3a891f115d59a375226a796b73943315273d9
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2022-01-05 13:14:02 +0900

    ビュー

---
+++ resources/views/practices/practice.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset=”UTF-8″>
+        <link rel="stylesheet" type="text/css" href="/css/practice.css">
+        <title>practice.blade.php</title>
+    </head>
+    <body>
+        <div class="p_header">あいうえお</div>
+    </body>
+</html>
